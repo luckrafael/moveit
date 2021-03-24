21c9848c2b66dbb19ade28313879537cbcd7f93e
luckrafael <lrs.rafael@hotmail.com>
2021-03-24 17:19:08 -0300

    fixing bug

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -5,13 +5,14 @@ import {
   useEffect,
   useState,
 } from "react";
+
 import { ChallengesContext } from "./ChallengesContext";
 
 interface CountdownContextData {
   minutes: number;
   seconds: number;
-  hasFinished: boolean;
   isActive: boolean;
+  hasFinished: boolean;
   startCountdown: () => void;
   resetCountdown: () => void;
 }
@@ -20,14 +21,13 @@ interface CountdownProviderProps {
   children: ReactNode;
 }
 
-export const CountdownContext = createContext({} as CountdownContextData);
-
 let countdownTimeout: NodeJS.Timeout;
 
-export function CountdownProvider({ children }: CountdownProviderProps) {
-  const { startNewChallenge } = useContext(ChallengesContext);
+export const CountdownContext = createContext({} as CountdownContextData);
 
-  const [time, setTime] = useState(0.05 * 60);
+export const CountdownProvider = ({ children }: CountdownProviderProps) => {
+  const { startNewChallenge } = useContext(ChallengesContext);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -41,13 +41,13 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
   useEffect(() => {
     if (isActive && time > 0) {
-      setTimeout(() => {
+      countdownTimeout = setTimeout(() => {
         setTime(time - 1);
       }, 1000);
     } else if (isActive && time === 0) {
@@ -55,15 +55,15 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
       setIsActive(false);
       startNewChallenge();
     }
-  }, [isActive, time]);
+  }, [isActive, time, startNewChallenge]);
 
   return (
     <CountdownContext.Provider
       value={{
         minutes,
         seconds,
-        hasFinished,
         isActive,
+        hasFinished,
         startCountdown,
         resetCountdown,
       }}
@@ -71,4 +71,4 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
       {children}
     </CountdownContext.Provider>
   );
-}
+};
